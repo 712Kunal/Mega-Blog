43e133d2a4398c486ebdb328ecb0c31de796ba07
712Kunal <jadhavkun0@gmail.com>
2024-07-06 22:16:34 +0530

    edited some tailwind file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [react()],
 })
